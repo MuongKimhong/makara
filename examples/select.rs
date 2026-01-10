ad385a3a705e2d551e3a133207894622d4ab731a
MuongKimhong <muongkimhong086@gmail.com>
2026-01-11 10:59:57 +1100

    add select example

---
+++ examples/select.rs
@@ -0,0 +1,41 @@
+use makara::prelude::*;
+use bevy::prelude::*;
+
+fn main() {
+    App::new()
+        .add_plugins(MakaraPlugin::default())
+        .add_systems(Startup, setup)
+        .run();
+}
+
+fn setup(mut commands: Commands) {
+    // bevy needs this
+    commands.spawn(Camera2d);
+
+    commands.spawn((
+        root()
+            .align_items(AlignItems::Center)
+            .justify_content(JustifyContent::Center)
+            .build(),
+
+        children![
+            text("").id("selected-choice").build(),
+
+            (
+                select(
+                    "Select your",
+                    &["Ford Falcon XR6", "Ford Mustang GT", "Holden Commodore Z6", "Subaru WRX"]
+                )
+                .width(px(200))
+                .margin_top(px(10))
+                .build(),
+
+                observe(|change: On<Change<String>>, mut text_q: TextQuery| {
+                    if let Some(text) = text_q.find_by_id("selected-choice") {
+                        text.text.value.0 = change.data.clone();
+                    }
+                })
+            )
+        ]
+    ));
+}
