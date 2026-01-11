abca214df9f285e91ee10104a88876ff4b5643c8
MuongKimhong <muongkimhong086@gmail.com>
2026-01-11 11:06:47 +1100

    add dropdown example

---
+++ examples/dropdown.rs
@@ -0,0 +1,45 @@
+use makara::prelude::*;
+use bevy::prelude::*;
+
+fn main() {
+    App::new()
+        .add_plugins(MakaraPlugin::default())
+        .add_systems(Startup, setup)
+        .run();
+}
+
+fn setup(mut commands: Commands) {
+    // bevy needs this
+    commands.spawn(Camera2d);
+
+    commands.spawn((
+        root()
+            .id("root")
+            .align_items(AlignItems::Center)
+            .justify_content(JustifyContent::Center)
+            .build(),
+
+        children![
+            (
+                dropdown("Background color options").margin_top(px(10)).build(),
+
+                children![
+                    (
+                        button("Red").build(),
+                        observe(|_clicked: On<Clicked>, mut root_q: RootQuery| {
+                            let root = root_q.find_by_id("root").unwrap();
+                            root.style.background_color.0 = Color::srgb(1.0, 0.0, 0.0);
+                        })
+                    ),
+                    (
+                        button("Blue").build(),
+                        observe(|_clicked: On<Clicked>, mut root_q: RootQuery| {
+                            let root = root_q.find_by_id("root").unwrap();
+                            root.style.background_color.0 = Color::srgb(0.0, 0.0, 1.0);
+                        })
+                    )
+                ]
+            )
+        ]
+    ));
+}
